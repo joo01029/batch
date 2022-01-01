23be7343362106c9180819ded7ed89c8f2ae0ae3
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-01-01 15:45:04 +0900

    [add] 동일한 JobParameter사용 테스트

--- src/main/java/com/example/batch/jobs/TutorialConfig.java
+++ src/main/java/com/example/batch/jobs/TutorialConfig.java
@@ -1,76 +1,77 @@
-package com.example.batch.jobs;
-
-import com.example.batch.tasklets.TutorialTasklet;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
-@Slf4j
-@Configuration
-@RequiredArgsConstructor
-public class TutorialConfig {
-	private final JobBuilderFactory jobBuilderFactory;
-	private final StepBuilderFactory stepBuilderFactory;
-
-	@Bean
-	@Qualifier("tutorialJob")
-	public Job tutorialJob(){
-		return jobBuilderFactory.get("tutorialJob")
-				.start(tutorialStep())
-				.build();
-	}
-
-	@Bean
-	public Step tutorialStep(){
-		return stepBuilderFactory.get("tutorialStep")
-				.tasklet(new TutorialTasklet())
-				.build();
-	}
-
-
-	@Bean
-	@Qualifier("multiJob")
-	public Job multiJob(){
-		return jobBuilderFactory.get("MultiSteps")
-				.start(firstStep())
-				.next(secondStep())
-				.next(thirdStep())
-				.build();
-	}
-
-	@Bean
-	public Step firstStep(){
-		return stepBuilderFactory.get("FirstStep")
-				.tasklet((contribution, chunckContext)->{
-					log.info("First Step!!");
-					return RepeatStatus.FINISHED;
-				})
-				.build();
-	}
-	@Bean
-	public Step secondStep(){
-		return stepBuilderFactory.get("SecondStep")
-				.tasklet((contribution, chunckContext)->{
-					log.info("Second Step!!");
-					return RepeatStatus.FINISHED;
-				})
-				.build();
-	}
-	@Bean
-	public Step thirdStep(){
-		return stepBuilderFactory.get("ThirdStep")
-				.tasklet((contribution, chunckContext)->{
-					log.info("Third Step!!");
-					return RepeatStatus.FINISHED;
-				})
-				.build();
-	}
-}
+//package com.example.batch.jobs;
+//
+//import com.example.batch.tasklets.TutorialTasklet;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Slf4j
+//@Configuration
+//@RequiredArgsConstructor
+//public class TutorialConfig {
+//	private final JobBuilderFactory jobBuilderFactory;
+//	private final StepBuilderFactory stepBuilderFactory;
+//
+//	@Bean
+//	@Qualifier("tutorialJob")
+//	public Job tutorialJob(){
+//		return jobBuilderFactory.get("tutorialJob")
+//				.preventRestart()
+//				.start(tutorialStep())
+//				.build();
+//	}
+//
+//	@Bean
+//	public Step tutorialStep(){
+//		return stepBuilderFactory.get("tutorialStep")
+//				.tasklet(new TutorialTasklet())
+//				.build();
+//	}
+//
+//
+//	@Bean
+//	@Qualifier("multiJob")
+//	public Job multiJob(){
+//		return jobBuilderFactory.get("MultiSteps")
+//				.start(firstStep())
+//				.next(secondStep())
+//				.next(thirdStep())
+//				.build();
+//	}
+//
+//	@Bean
+//	public Step firstStep(){
+//		return stepBuilderFactory.get("FirstStep")
+//				.tasklet((contribution, chunckContext)->{
+//					log.info("First Step!!");
+//					return RepeatStatus.FINISHED;
+//				})
+//				.build();
+//	}
+//	@Bean
+//	public Step secondStep(){
+//		return stepBuilderFactory.get("SecondStep")
+//				.tasklet((contribution, chunckContext)->{
+//					log.info("Second Step!!");
+//					return RepeatStatus.FINISHED;
+//				})
+//				.build();
+//	}
+//	@Bean
+//	public Step thirdStep(){
+//		return stepBuilderFactory.get("ThirdStep")
+//				.tasklet((contribution, chunckContext)->{
+//					log.info("Third Step!!");
+//					return RepeatStatus.FINISHED;
+//				})
+//				.build();
+//	}
+//}
--- src/main/java/com/example/batch/schedulers/TutorialScheduler.java
+++ src/main/java/com/example/batch/schedulers/TutorialScheduler.java
@@ -1,51 +1,51 @@
-package com.example.batch.schedulers;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class TutorialScheduler {
-	private final Job multiJob;
-	private final Job tutorialJob;
-	private final JobLauncher jobLauncher;
-
-	@Scheduled(fixedDelay = 50*1000L)
-	public void executeJob(){
-		try{
-			JobParameters parameter = new JobParametersBuilder()
-					.addString("datetime", LocalDateTime.now().toString())
-					.toJobParameters();
-			jobLauncher.run(
-					multiJob,
-					parameter
-			);
-		}catch (JobExecutionException e){
-			log.error(e.getMessage());
-			e.printStackTrace();
-		}
-	}
-
-	@Scheduled(fixedDelay = 5*1000L)
-	public void executeJob2(){
-		try{
-			JobParameters parameter = new JobParametersBuilder()
-					.addString("datetime", LocalDateTime.now().toString())
-					.toJobParameters();
-			jobLauncher.run(
-					tutorialJob,
-					parameter
-			);
-		}catch (JobExecutionException e){
-			log.error(e.getMessage());
-			e.printStackTrace();
-		}
-	}
-}
+//package com.example.batch.schedulers;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.launch.JobLauncher;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class TutorialScheduler {
+//	private final Job multiJob;
+////	private final Job tutorialJob;
+//	private final JobLauncher jobLauncher;
+//
+//	@Scheduled(fixedDelay = 5*1000L)
+//	public void executeJob(){
+//		try{
+//			JobParameters parameter = new JobParametersBuilder()
+//					.addString("datetime", LocalDateTime.now().toString())
+//					.toJobParameters();
+//			jobLauncher.run(
+//					multiJob,
+//					parameter
+//			);
+//		}catch (JobExecutionException e){
+//			log.error(e.getMessage());
+//			e.printStackTrace();
+//		}
+//	}
+//
+////	@Scheduled(fixedDelay = 5*1000L)
+////	public void executeJob2(){
+////		try{
+////			JobParameters parameter = new JobParametersBuilder()
+////					.addString("datetime", LocalDateTime.now().toString())
+////					.toJobParameters();
+////			jobLauncher.run(
+////					tutorialJob,
+////					parameter
+////			);
+////		}catch (JobExecutionException e){
+////			log.error(e.getMessage());
+////			e.printStackTrace();
+////		}
+////	}
+//}
